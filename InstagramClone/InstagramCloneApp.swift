0d01ba0e9e54bc7ed42574385d7a42bfd60480fa
Guyvenson <createwithcodes@gmail.com>
2024-05-07 17:06:31 -0400

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Jean Racine on 5/7/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
